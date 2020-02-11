000464a6e5a61180db31aef87dfb681d8238c163
Jesse Chung <jesse@soob.co>
2020-02-11 17:41:20 +0900

    Updates

---
+++ precache-manifest.522c0e977d69a471c26e1aea1a08bc9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd516ee2d3f8c3bebb8b84115085c5e9",
+    "url": "/voting-app-frontend/index.html"
+  },
+  {
+    "revision": "c8847a7ee901e04821df",
+    "url": "/voting-app-frontend/static/css/main.21c838c7.chunk.css"
+  },
+  {
+    "revision": "9dfda4013cf77c44b635",
+    "url": "/voting-app-frontend/static/js/2.3bc57c5c.chunk.js"
+  },
+  {
+    "revision": "7684ce238112768d77dc47c486d3fab8",
+    "url": "/voting-app-frontend/static/js/2.3bc57c5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8847a7ee901e04821df",
+    "url": "/voting-app-frontend/static/js/main.8a478aa6.chunk.js"
+  },
+  {
+    "revision": "85cb1e2b7b801fd39af0",
+    "url": "/voting-app-frontend/static/js/runtime-main.14d59fbd.js"
+  }
+]);
